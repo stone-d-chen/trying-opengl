f12a71503edb7233f915c76f0651b482c8264cea
Stone Chen <chen.stonechen@gmail.com>
2023-07-19 20:07:53 -0400

    attributes

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-cl.exe /O2 sdl_ogl.cpp  /link lib\SDL2main.lib lib\SDL2.lib "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22000.0\um\x64\shell32.lib" /SUBSYSTEM:CONSOLE /OUT:lib/sdl_ogl.exe
+cl.exe /Zi sdl_ogl.cpp  /link lib\SDL2main.lib lib\SDL2.lib "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22000.0\um\x64\shell32.lib" /SUBSYSTEM:CONSOLE /OUT:lib/sdl_ogl.exe
--- sdl_ogl.cpp
+++ sdl_ogl.cpp
@@ -1,9 +1,16 @@
 #include "include/SDL.h"
+#include "include/SDL_opengl.h"
 #include <stdio.h>
+#include <iostream>
 
-#define global_variable static
+#define global static
 
-global_variable bool is_running = false;
+global bool is_running = false;
+global int Width = 640;
+global int Height = 480;
+
+SDL_Window *Window;
+SDL_GLContext Context;
 
 int main(int ArgC, char **Args)
 {
@@ -14,8 +21,25 @@ int main(int ArgC, char **Args)
         printf("running");
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
+    Window = SDL_CreateWindow("OGL Window", 0, 0, Width, Height, SDL_WINDOW_OPENGL);
+    Context = SDL_GL_CreateContext(Window);
+    if(Context == NULL)
+    {
+        std::cout << "Context problem" << std::endl;
+        return(1);
+    }
+
     while(is_running == true)
     {
+        static int counter = 0;
+        counter += 1;
+        if(counter > 10000) is_running = false;
     }
 
 
