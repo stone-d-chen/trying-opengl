8eeac5054a75a0cc8758263fc5f547c07c867bef
Stone Chen <chen.stonechen@gmail.com>
2023-07-19 20:34:39 -0400

    opengl in SDL2 runing

--- src/sdl_ogl.cpp
+++ src/sdl_ogl.cpp
@@ -1,7 +1,9 @@
-#include "include/SDL.h"
-#include "include/SDL_opengl.h"
+#include "glad/glad.h"
+#include "SDL.h"
+#include "SDL_opengl.h"
 #include <stdio.h>
 #include <iostream>
+
 
 #define global static
 
@@ -11,6 +13,14 @@ global int Height = 480;
 
 SDL_Window *Window;
 SDL_GLContext Context;
+
+void GetOpenGLInfo()
+{
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl; 
+    std::cout << "Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "Shading Lang: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+}
 
 int main(int ArgC, char **Args)
 {
@@ -27,8 +37,17 @@ int main(int ArgC, char **Args)
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
+
     Window = SDL_CreateWindow("OGL Window", 0, 0, Width, Height, SDL_WINDOW_OPENGL);
     Context = SDL_GL_CreateContext(Window);
+
+    if(!gladLoadGLLoader(SDL_GL_GetProcAddress)) 
+    {
+        std::cout << "LoaderGLLLoader did not work" << std::endl;
+    }
+    
+    GetOpenGLInfo();
+
     if(Context == NULL)
     {
         std::cout << "Context problem" << std::endl;
@@ -37,9 +56,11 @@ int main(int ArgC, char **Args)
 
     while(is_running == true)
     {
-        static int counter = 0;
-        counter += 1;
-        if(counter > 10000) is_running = false;
+        SDL_Event e;
+        while(SDL_PollEvent(&e) != 0)
+            if(e.type == SDL_QUIT) is_running = false;
+
+
     }
 
 
